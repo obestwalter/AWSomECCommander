1b0580286598b7595301b77f32be76f9577a3acc
Oliver Bestwalter <oliver@bestwalter.de>
2014-11-11 14:54:55 +0100

    pep8 line length

--- beeswithmachineguns/main.py
+++ beeswithmachineguns/main.py
@@ -191,8 +191,8 @@ commands:
         if options.group == 'default':
             print 'New bees will use the "default" EC2 security group. ' \
                   'Please note that port 22 (SSH) is not normally open on ' \
-                  'this group. You will need to use to the EC2 tools to open ' \
-                  'it before you will be able to attack.'
+                  'this group. You will need to use to the EC2 tools to ' \
+                  'open it before you will be able to attack.'
         bees.up(options.servers, options.group, options.zone, options.instance,
                 options.type, options.login, options.key, options.subnet)
     elif command == 'attack':
